09140c43a1f9641f71f94e0c873c68af325fb7bc
Florian <105889597+florian-chicot@users.noreply.github.com>
2023-01-20 18:54:45 +0100

    Add suggestions for search textfield

--- lib/UI/home.dart
+++ lib/UI/home.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:projet_flutter/UI/aboutUs.dart';
 import 'package:projet_flutter/UI/search.dart';
 
+import '../Model/country.dart';
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+
 class App extends StatefulWidget {
   const App({super.key});
 
@@ -11,6 +17,31 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   final TextEditingController _controller = TextEditingController();
+  final TextEditingController _typeAheadController = TextEditingController();
+  List<String> _countryNames = [];
+  late Future<List<String>> getCountryNames;
+
+  Future<List<String>> _getCountryNames() async {
+    final response = await http.get(Uri.parse('https://restcountries.com/v3.1/all'));
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response, then parse the JSON.
+      List<dynamic> countriesJson = json.decode(response.body);
+      List<Country> countries = countriesJson.map((c) => Country.fromJson(c)).toList();
+      List<String> _countryNames = countries.map((c) => c.name).toList();
+      return _countryNames;
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load countries');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCountryNames();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,12 +61,25 @@ class _AppState extends State<App> {
               children: <Widget>[
                 SizedBox(
                   width: 300,
-                  child: TextFormField(
-                    controller: _controller,
-                    decoration: const InputDecoration(
-                      border: UnderlineInputBorder(),
-                      labelText: 'Search Countries',
+                  child: TypeAheadField(
+                    textFieldConfiguration: TextFieldConfiguration(
+                      controller: _typeAheadController,
+                      decoration: InputDecoration(
+                        labelText: 'Country',
+                      ),
                     ),
+                    suggestionsCallback: (pattern) async {
+                      _countryNames = await _getCountryNames();
+                      return _countryNames.where((name) => name.toLowerCase().startsWith(pattern.toLowerCase())).toList();
+                    },
+                    itemBuilder: (context, suggestion) {
+                      return ListTile(
+                        title: Text(suggestion),
+                      );
+                    },
+                    onSuggestionSelected: (suggestion) {
+                      _typeAheadController.text = suggestion;
+                    },
                   ),
                 ),
                 ElevatedButton(
