774e82ac43d78e33da4da45fc75a2d5af35f65b4
Florian <105889597+florian-chicot@users.noreply.github.com>
2023-01-21 12:03:03 +0100

    Update countryDetail view to scrollable view

--- lib/UI/countryDetail.dart
+++ lib/UI/countryDetail.dart
@@ -75,74 +75,76 @@ class _CountryDetailState extends State<CountryDetail> {
         ),
       ),
       body: Center (
-        child : Container(
-          padding: const EdgeInsets.all(2),
-          child: Column(
-            children: <Widget>[
-              Image.network(
-                country.flag,
-                width: 500,
-              ),
-              Text(country.name,
-                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-              const Padding(
-                padding: EdgeInsets.only(top:16), //apply padding horizontal or vertical only
-                child: Text("Official name",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-              ),
-              Text(country.officialName,
-                  style: const TextStyle(fontSize: 16)),
-              const Padding(
-                padding: EdgeInsets.only(top:16), //apply padding horizontal or vertical only
-                child: Text("Name in French",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-              ),
-              Text(country.frenchName,
-                  style: const TextStyle(fontSize: 16)),
-              const Padding(
-                padding: EdgeInsets.only(top:16), //apply padding horizontal or vertical only
-                child: Text("Continent",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-              ),
-              Text(country.region,
-                  style: const TextStyle(fontSize: 16)),
-              const Padding(
-                padding: EdgeInsets.only(top:16), //apply padding horizontal or vertical only
-                child: Text("Population",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-              ),
-              Text(country.population.toString()+" inhabitants",
-                  style: const TextStyle(fontSize: 16)),
-              Padding(
-                padding: EdgeInsets.only(top:16), //apply padding horizontal or vertical only
-                child: Text(country.capital.length == 1 ? "Capital city" : "Capital cities",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-              ),
-              Text(country.capital.join(', '),
-                  style: const TextStyle(fontSize: 16)),
-              Padding(
-                padding: EdgeInsets.only(top:16), //apply padding horizontal or vertical only
-                child: Text(country.languages.length == 1 ? "Official language" : "Official languages",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-              ),
-              Text(country.languages.join(', '),
-                  style: const TextStyle(fontSize: 16)),
-              Padding(
-                padding: EdgeInsets.only(top:16), //apply padding horizontal or vertical only
-                child: Text(country.currencies.length == 1 ? "Currency" : "Currencies",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-              ),
-              Text(country.currencies.join(', '),
-                  style: const TextStyle(fontSize: 16)),
-              Padding(
-                padding: EdgeInsets.only(top:16), //apply padding horizontal or vertical only
-                child: Text(country.topLevelDomains.length == 1 ? "Top-level domain" : "Top-level domains",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-              ),
-              Text(country.topLevelDomains.join(', '),
-                  style: const TextStyle(fontSize: 16)),
-            ],
-          ),
+        child: ListView(
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.network(
+                  country.flag,
+                  width: 500,
+                ),
+                Text(country.name,
+                  style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                const Padding(
+                  padding: EdgeInsets.only(top:16), //apply padding horizontal or vertical only
+                  child: Text("Official name",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                ),
+                Text(country.officialName,
+                    style: const TextStyle(fontSize: 16)),
+                const Padding(
+                  padding: EdgeInsets.only(top:16), //apply padding horizontal or vertical only
+                  child: Text("Name in French",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                ),
+                Text(country.frenchName,
+                    style: const TextStyle(fontSize: 16)),
+                const Padding(
+                  padding: EdgeInsets.only(top:16), //apply padding horizontal or vertical only
+                  child: Text("Continent",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                ),
+                Text(country.region,
+                    style: const TextStyle(fontSize: 16)),
+                const Padding(
+                  padding: EdgeInsets.only(top:16), //apply padding horizontal or vertical only
+                  child: Text("Population",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                ),
+                Text(country.population.toString()+" inhabitants",
+                    style: const TextStyle(fontSize: 16)),
+                Padding(
+                  padding: EdgeInsets.only(top:16), //apply padding horizontal or vertical only
+                  child: Text(country.capital.length == 1 ? "Capital city" : "Capital cities",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                ),
+                Text(country.capital.join(', '),
+                    style: const TextStyle(fontSize: 16)),
+                Padding(
+                  padding: EdgeInsets.only(top:16), //apply padding horizontal or vertical only
+                  child: Text(country.languages.length == 1 ? "Official language" : "Official languages",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                ),
+                Text(country.languages.join(', '),
+                    style: const TextStyle(fontSize: 16)),
+                Padding(
+                  padding: EdgeInsets.only(top:16), //apply padding horizontal or vertical only
+                  child: Text(country.currencies.length == 1 ? "Currency" : "Currencies",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                ),
+                Text(country.currencies.join(', '),
+                    style: const TextStyle(fontSize: 16)),
+                Padding(
+                  padding: EdgeInsets.only(top:16), //apply padding horizontal or vertical only
+                  child: Text(country.topLevelDomains.length == 1 ? "Top-level domain" : "Top-level domains",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                ),
+                Text(country.topLevelDomains.join(', '),
+                    style: const TextStyle(fontSize: 16)),
+              ],
+            ),
+          ],
         ),
       ),
     );
